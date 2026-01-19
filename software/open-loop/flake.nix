0c1821f33f9fa3a69dfa4cbfea3483c0b86c4c25
samuel-sjoden <sjodensamuel@gmail.com>
2026-01-19 09:21:48 -0800

    added flake for open-loop

---
+++ software/open-loop/flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "ESP-IDF development shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs-esp-dev = {
+      url = "github:mirrexagon/nixpkgs-esp-dev";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-esp-dev }:
+    let
+      system = "x86_64-linux"; # adjust if needed
+
+      pkgs = import nixpkgs {
+        inherit system;
+
+        overlays = [
+          (import "${nixpkgs-esp-dev}/overlay.nix")
+        ];
+
+        config.permittedInsecurePackages = [
+          "python3.13-ecdsa-0.19.1"
+        ];
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        name = "esp-project";
+
+        buildInputs = with pkgs; [
+          esp-idf-full
+        ];
+      };
+    };
+}
+
